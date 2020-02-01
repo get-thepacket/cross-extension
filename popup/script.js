929af0f11512273b8f1ef6993311610a4b439051
get-thepacket <lakshyab.1999@gmail.com>
2020-02-01 20:00:30 +0530

    Basic template commit

---
+++ popup/script.js
@@ -0,0 +1,27 @@
+var oReq = new XMLHttpRequest();
+oReq.open("GET", "http://api.icndb.com/jokes/random/");
+oReq.send();
+oReq.onreadystatechange = function()
+{
+    if(oReq.readyState == 4)
+    {
+        var target = document.getElementById("itsAFact");
+        if(oReq.status == 200)
+        {
+            try{
+
+                var response=(JSON.parse(oReq.responseText).value.joke);
+
+                target.innerText = response;
+
+            }catch(e){
+                target.innerHTML = "Oops, something went wrong :( ";    
+            }
+            
+        }
+        else
+            target.innerHTML = "Oops, something went wrong :( ";
+
+        target.style="background: #b8b898;";
+    }
+};
